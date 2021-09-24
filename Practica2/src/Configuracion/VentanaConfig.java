b10839c5224d922e0273caaeeb275b39c23ecdf3
Rodrigo Hernández <2823897250101@ingenieria.usac.edu.gt>
2021-09-23 23:01:38 -0600

    Interfaz Grafica Configuración

--- Practica2/src/Configuracion/VentanaConfig.java
+++ Practica2/src/Configuracion/VentanaConfig.java
@@ -1,15 +1,81 @@
 package Configuracion;
 
+import Principal.Principal;
 import java.awt.Color;
+import java.awt.Font;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
 
 public class VentanaConfig extends JFrame implements ActionListener{
     Color fondoc = new Color(79,201,247);
+    JComboBox cdiscos;
+    JTextField tftiempo;
+    JLabel titulo, ldiscos, ltiempo;
+    JButton guardar, salir;
     
     //CONSTRUCTOR
-    public VentanaConfig(){        
+    public VentanaConfig(){
+        //JLABEL DE TITULO
+        titulo = new JLabel("Configuración");
+        titulo.setBounds(400,50,300,40);
+        titulo.setFont(new Font("Century Gothic", Font.BOLD,30));
+        titulo.setVisible(true);
+        this.add(titulo);
+        
+        //JLALBEL DE DISCOS
+        ldiscos = new JLabel("Cantidad de discos");
+        ldiscos.setBounds(150,150,350,30);
+        ldiscos.setFont(new Font("Century Gothic", Font.BOLD,25));
+        this.add(ldiscos);
+        
+        //COMBOBOX DE LA CANTIDAD DE DISCOS
+        int[] cantidad = {3,4,5,6,7};
+        cdiscos = new JComboBox();
+        for (int i = 0; i < cantidad.length; i++) {
+            cdiscos.addItem(cantidad[i]);
+        }
+        cdiscos.setFont(new Font("Century Gothic", Font.PLAIN,15));
+        cdiscos.setVisible(true);
+        cdiscos.setBounds(100,200,350,30);
+        this.add(cdiscos);
+        
+        //JLALBEL DE TIEMPO
+        ltiempo = new JLabel("Tiempo de partida");
+        ltiempo.setBounds(600,150,350,30);
+        ltiempo.setFont(new Font("Century Gothic", Font.BOLD,25));
+        this.add(ltiempo);
+        
+        //TEXTFIELD DEL TIEMPO
+        tftiempo = new JTextField();
+        tftiempo.setBounds(550,200,350,30);
+        tftiempo.setVisible(true);
+        tftiempo.setFont(new Font("Century Gothic", Font.PLAIN,15));
+        this.add(tftiempo);
+        
+        //BOTON DE GUARDAR
+        Image imgeb10 = new ImageIcon("Imagenes/Boton10.png").getImage();
+        ImageIcon b10 = new ImageIcon(imgeb10.getScaledInstance(200, 50, Image.SCALE_SMOOTH));
+        guardar = new JButton(b10);
+        guardar.setBorder(null);
+        guardar.setLayout(null);
+        guardar.setBounds(400,400, 200, 50);
+        guardar.setVisible(true);
+        guardar.addActionListener(this);
+        this.add(guardar);
+        
+        //BOTON DE SALIR
+        Image imgeb5 = new ImageIcon("Imagenes/Boton5.png").getImage();
+        ImageIcon b5 = new ImageIcon(imgeb5.getScaledInstance(140, 50, Image.SCALE_SMOOTH));
+        salir = new JButton(b5);
+        salir.setBorder(null);
+        salir.setLayout(null);
+        salir.setBounds(800,400, 100, 40);
+        salir.setVisible(true);
+        salir.addActionListener(this);
+        this.add(salir);
+        
         //ICONO DE LA APLICACION
         setIconImage(new ImageIcon(getClass().getResource("torre.png")).getImage());
         
@@ -26,6 +92,9 @@ public class VentanaConfig extends JFrame implements ActionListener{
     //METODOS ABSTRACTOS
     @Override
     public void actionPerformed(ActionEvent ae) {
-        
+        if (ae.getSource()==salir) {
+            Principal p = new Principal();
+            this.dispose();
+        }
     }    
 }
